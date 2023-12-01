445522cb99fffd007a36a0d00ec0ffbbd3c080b1
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-12-01 11:35:35 +0100

    Gestion de l'URL du poster du film

--- src/movies-api.js
+++ src/movies-api.js
@@ -1,5 +1,14 @@
-export const API_URL='http://movies-api';
+export const API_URL = "http://movies-api";
+export const IMAGE_SIZES = ["original", "xsmall", "small", "medium"];
 
-export function getAllMovies(){
-  return fetch(`${API_URL}/movies`).then((response)=>response.json())
+export function getAllMovies() {
+  return fetch(`${API_URL}/movies`).then((response) => response.json());
+}
+
+export function posterUrl(imagePath, size = "original") {
+  /*
+With imagePath the path given by API,
+and the size can be : « xsmall », « small », « medium » or « original » : last for default
+ */
+  return `${API_URL}${imagePath}/${size}`;
 }
