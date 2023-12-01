c616bd6c039d11c3ff76cd7b878ee2929e4e1231
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-12-01 11:37:10 +0100

    Affichage du poster du film

--- src/movies-ui.js
+++ src/movies-ui.js
@@ -1,9 +1,15 @@
-import { getAllMovies } from "./movies-api";
+import { getAllMovies, posterUrl } from "./movies-api";
 
 export function createMovieElt(movieData) {
   const movieElt = document.createElement("article");
   movieElt.className = "movie-item";
-  movieElt.innerHTML = `<div class="movie-item__info"><div class="movie-item__title">${movieData.title}</div></div>`;
+  movieElt.innerHTML = `<img src="${posterUrl(
+    movieData.poster,
+    "medium",
+  )}" alt="poster of '${movieData.title}'" class="movie-item__poster">
+<div class="movie-item__info"><div class="movie-item__title">${
+    movieData.title
+  }</div></div>`;
   return movieElt;
 }
 
